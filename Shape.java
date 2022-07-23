3cc0c3a032b0d001df9561058abc50eef407d689
SanketBansod174 <sanketbansod23@gmail.com>
2022-07-23 15:30:53 +0530

    new demo testing

---
+++ Shape.java
@@ -0,0 +1,31 @@
+package shapes;
+
+public abstract class Shape {
+	private int x, y;
+        private String color;
+	private int width;
+	public Shape(int x, int y) {
+		// super();
+		this.x = x;
+		this.y = y;
+	}
+	
+  public abstract void draw();
+
+
+
+
+	@Override
+	public String toString() {
+		System.out.println("in shape's tostring");
+	//	System.out.println(super.toString());
+		return x + " " + y;
+	}
+	//area
+	public double area()
+	{ // calculate area of shape.
+		System.out.println("un defined area");
+		return -1;
+	}
+
+}
