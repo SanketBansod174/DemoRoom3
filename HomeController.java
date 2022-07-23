ed2ac7706282828258a2f548205f85c592d6b66f
JuleePGunjal <gunjaljulee25@gmail.com>
2022-07-23 15:59:16 +0530

    added some in dayThree

--- HomeController.java
+++ HomeController.java
@@ -1,3 +1,4 @@
+//in dayThree changes done
 package com.app.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,7 +11,9 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String shoeIndexPage() {
+		System.out.println("====Added some comment to check the whether data added to dayThree branch====");
 		return "/index";
+
 	}
 
 }
